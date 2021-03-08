6b065242af04253335dbbedfd5c3868ffb51ee81
Positive-Conative <kyj970513@naver.com>
2021-03-08 15:50:07 +0900

    Edit : research results paging

--- public/javascripts/research_fields/researchFieldsMain.js
+++ public/javascripts/research_fields/researchFieldsMain.js
@@ -2,6 +2,11 @@
 
 var pageQuery = location.search;
 var queryParams = new URLSearchParams(pageQuery);
+// function privX2page(){
+//     if(queryParams.get('page') <= 7) queryParams.set('page', 1);
+//     else queryParams.set('page', queryParams.get('page')-7);
+//     location.search = queryParams.toString();
+// }
 function privpage(){
     queryParams.set('page', queryParams.get('page')-1);
     location.search = queryParams.toString();
@@ -14,3 +19,8 @@ function nextpage(){
     queryParams.set('page', Number(queryParams.get('page'))+1);
     location.search = queryParams.toString();
 }
+// function nextX2page(){
+//     if(queryParams.get('page') <= <%=Math.ceil(db_data.length / 5 )%>-7) queryParams.set('page', <%=Math.ceil(db_data.length / 5 )%>);
+//     else queryParams.set('page', Number(queryParams.get('page'))+7);
+//     location.search = queryParams.toString();
+// }
