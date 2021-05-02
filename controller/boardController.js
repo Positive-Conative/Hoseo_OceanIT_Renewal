6f657839cc3dbd5a738ce84e07547de7c55264e8
sun980120 <70737532+sun980120@users.noreply.github.com>
2021-05-02 20:50:13 +0900

    Router : Notice_BoardWrite\nForm Change

--- controller/boardController.js
+++ controller/boardController.js
@@ -1,62 +1,104 @@
 'use strict';
 
+var dayjs = require('dayjs');
+const db = require('../config/kyjdb');
 var jwtmiddle = require('../middleware/jwt');
 var boardDAO = require('../model/boardDAO');
+var logger = require('../config/logger');
 
 function noticeMain(req, res, next) {
-    let token = req.cookies.user;
-    jwtmiddle.jwtCerti(token).then(
-        (permission)=>{
-            res.render('board/notice/noticeMain', { permission })
+    boardDAO.count_noticeBoard().then(
+        (db_data) => {
+            let token = req.cookies.user;
+            jwtmiddle.jwtCerti(token).then(
+                (permission) => {
+                    console.log("user_id : " + permission.user_id)
+                    console.log("title : "+ req.body.title)
+                    res.render('board/notice/noticeMain', { db_data, dayjs, permission })
+                }
+            ).catch(err => res.send("<script>alert('jwt err');</script>"));
         }
-    ).catch(err=>res.send("<script>alert('jwt err');</script>"));
+    ).catch(err => res.send("<script>alert('" + err + "');location.href='/';</script>"))
 }
 function noticeWrite(req, res, next) {
     let token = req.cookies.user;
     jwtmiddle.jwtCerti(token).then(
-        (permission)=>{
+        (permission) => {
             res.render('board/notice/noticeWrite', { permission })
         }
-    ).catch(err=>res.send("<script>alert('jwt err');</script>"));
+    ).catch(err => res.send("<script>alert('jwt err');</script>"));
 }
-
-function inquiryMain(req, res, next) {
+function noticeWritePost(req, res, next) {
+    var content = req.body.content;
+    var title = req.body.title;
+    console.log("title : " + title);
+    console.log("content : " + content);
     let token = req.cookies.user;
     jwtmiddle.jwtCerti(token).then(
-        (permission)=>{
-            res.render('board/inquiry/inquiryMain', { permission })
+        (permission) => {
+            var date = new dayjs();
+            var datetime = date.format('YYYY-MM-DD HH:mm:ss');
+            var user_id = permission.user_id;
+            var datas = {user_id:user_id, title:title, content:content, date:datetime};
+            db.query('INSERT INTO Notice_Board SET ?', datas, function (error, row) {
+                //console.log("db_data : " + row)
+                if (error) {
+                    logger.error(
+                        "DB error [Notice_Board]" +
+                        "\n \t" + error);
+                    rejcet('DB ERR');}
+                else { res.redirect("/board/notice"); }
+            })
         }
-    ).catch(err=>res.send("<script>alert('jwt err');</script>"));
+    ).catch(err => res.send("<script>alert('jwt err');</script>"));
+}
+function inquiryMain(req, res, next) {
+    boardDAO.count_questionBoard().then(
+        (db_data) => {
+            let token = req.cookies.user;
+            jwtmiddle.jwtCerti(token).then(
+                (permission) => {
+                    res.render('board/inquiry/inquiryMain', { db_data, dayjs, permission })
+                }
+            ).catch(err => res.send("<script>alert('jwt err');</script>"));
+        }
+    ).catch(err => res.send("<script>alert('" + err + "');location.href='/';</script>"))
 }
 function inquiryWrite(req, res, next) {
     let token = req.cookies.user;
     jwtmiddle.jwtCerti(token).then(
-        (permission)=>{
+        (permission) => {
             res.render('board/inquiry/inquiryWrite', { permission })
         }
-    ).catch(err=>res.send("<script>alert('jwt err');</script>"));
+    ).catch(err => res.send("<script>alert('jwt err');</script>"));
 }
 
 function freeBoardMain(req, res, next) {
-    let token = req.cookies.user;
-    jwtmiddle.jwtCerti(token).then(
-        (permission)=>{
-            res.render('board/free/FreeBoardMain', { permission })
+    boardDAO.count_freeBoard().then(
+        (db_data) => {
+            let token = req.cookies.user;
+            jwtmiddle.jwtCerti(token).then(
+                (permission) => {
+                    res.render('board/free/FreeBoardMain', { db_data, dayjs, permission })
+                }
+            ).catch(err => res.send("<script>alert('jwt err');</script>"));
+
         }
-    ).catch(err=>res.send("<script>alert('jwt err');</script>"));
+    ).catch(err => res.send("<script>alert('" + err + "');location.href='/';</script>"))
 }
 function freeBoardWrite(req, res, next) {
     let token = req.cookies.user;
     jwtmiddle.jwtCerti(token).then(
-        (permission)=>{
+        (permission) => {
             res.render('board/free/FreeBoardWrite', { permission })
         }
-    ).catch(err=>res.send("<script>alert('jwt err');</script>"));
+    ).catch(err => res.send("<script>alert('jwt err');</script>"));
 }
 
 module.exports = {
     noticeMain,
     noticeWrite,
+    noticeWritePost,
     inquiryMain,
     inquiryWrite,
     freeBoardMain,
--- model/boardDAO.js
+++ model/boardDAO.js
@@ -5,11 +5,11 @@ var logger = require('../config/logger');
 
 function count_questionBoard(parameters) {
     return new Promise(function (resolve, rejcet) {
-        let queryData = `SELECT DATE(write_date) AS write_date, COUNT(write_date) AS count FROM Inquiry_Board group BY DATE(write_date) ORDER BY DATE(write_date) desc`;
+        let queryData = `SELECT * FROM Inquiry_Board ORDER BY write_date desc`;
         db.query(queryData, function (error, db_data) {
             if (error) {
                 logger.error(
-                    "DB error [Inquiry_Board]"+
+                    "DB error [Inquiry_Board]" +
                     "\n \t" + queryData +
                     "\n \t" + error);
                 rejcet('DB ERR');
@@ -19,6 +19,42 @@ function count_questionBoard(parameters) {
         });
     })
 }
+function count_noticeBoard(parameters) {
+    return new Promise(function (resolve, rejcet) {
+        let queryData = `SELECT * From Notice_Board ORDER BY date desc`;
+        db.query(queryData, function (error, db_data) {
+            console.log(db_data)
+            if (error) {
+                logger.error(
+                    "DB error [Notice_Board]" +
+                    "\n \t" + queryData +
+                    "\n \t" + error);
+                rejcet('DB ERR');
+                //throw error;
+            }
+            resolve(db_data);
+        })
+    })
+}
+function count_freeBoard(parameters){
+    return new Promise(function(resolve,rejcet){
+        let queryData = `SELECT * FROM Free_Board ORDER BY date desc`;
+        db.query(queryData, function (error, db_data) {
+            console.log(db_data)
+            if (error) {
+                logger.error(
+                    "DB error [Notice_Board]" +
+                    "\n \t" + queryData +
+                    "\n \t" + error);
+                rejcet('DB ERR');
+                //throw error;
+            }
+            resolve(db_data);
+        })
+    })
+}
 module.exports = {
-    count_questionBoard
+    count_questionBoard,
+    count_noticeBoard,
+    count_freeBoard
 }
