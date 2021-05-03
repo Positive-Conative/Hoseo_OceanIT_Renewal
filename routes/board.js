beec4a28d90528fde4984eced66e1bc0878c91c6
chanyoung2371 <78065638+chanyoung2371@users.noreply.github.com>
2021-05-03 17:50:27 +0900

    Create board_Detail{css,js}

--- routes/board.js
+++ routes/board.js
@@ -6,17 +6,20 @@ var boardController = require('../controller/boardController');
 //공지사항 게시판
 router.get('/notice', boardController.noticeMain);
 router.get('/notice/noticeWrite', boardController.noticeWrite);
+router.get('/notice/noticeDetail',boardController.noticeDetail);
 router.post('/notice/noticeWrite', boardController.noticeWritePost);
 
 //문의게시판(1:1)
 router.get('/inquiry', boardController.inquiryMain);
 router.get('/inquiry/inquiryWrite', boardController.inquiryWrite);
+router.get('/inquiry/inquiryDetail',boardController.inquiryDetail);
 router.post('/inquiry/inquiryWrite', boardController.inquiryWritePost);
 
 
 // 자유게시판
 router.get('/free', boardController.freeBoardMain);
 router.get('/free/freeBoardWrite', boardController.freeBoardWrite);
+router.get('/free/freeBoardDetail', boardController.freeBoardDetail);
 router.post('/free/freeBoardWrite', boardController.freeBoardWritePost);
 
 
