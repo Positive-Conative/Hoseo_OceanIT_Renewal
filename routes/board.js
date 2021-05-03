c2b00ed1726698ae96a7af85a85ab47d0f9cef63
sun980120 <70737532+sun980120@users.noreply.github.com>
2021-05-03 12:10:09 +0900

    test

--- routes/board.js
+++ routes/board.js
@@ -7,27 +7,6 @@ var boardController = require('../controller/boardController');
 router.get('/notice', boardController.noticeMain);
 router.get('/notice/noticeWrite', boardController.noticeWrite);
 router.post('/notice/noticeWrite', boardController.noticeWritePost);
-/*router.post('/notice/noticeWrite', function (req, res){
-    let token = req.cookies.user;
-    var content = req.body.content;
-    var title = req.body.title;
-    console.log(req.body);
-    jwtmiddle.jwtCerti(token).then(
-        (permission) => {
-            var date = new dayjs();
-            var datetime = date.format('YYYY-MM-DD');
-            var user_id = permission.user_id;
-            console.log("title : " + title)
-            console.log("content : " + content);
-            var datas = {user_id:user_id, title:title, content:content, date:datetime};
-            db.query('INSERT INTO Notice_Board SET ?', datas, function (error, row) {
-                console.log("db_data : " + row)
-                if (error) { throw error; }
-                else { res.redirect("/board/notice"); }
-            })
-        }
-    ).catch(err => res.send("<script>alert('jwt err');</script>"));
-})*/
 
 //문의게시판(1:1)
 router.get('/inquiry', boardController.inquiryMain);
