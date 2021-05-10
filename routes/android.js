9dbb07b77a1c975b7860e755e87f7230894f796d
sun980120 <70737532+sun980120@users.noreply.github.com>
2021-05-10 15:08:51 +0900

    android : router

--- routes/android.js
+++ routes/android.js
@@ -2,13 +2,20 @@ var express = require('express');
 var router = express.Router();
 var db = require('../config/kyjDB');
 
-router.get('/researchResultes',(req,res)=>{
-    var queryData = `SELECT * FROM Research_Resultes LIMIT 1 ORDER BY date desc `;
-    db.query(queryData,function(err,fields){
-        if(err){throw err;}
-        else{
-            res.send(fields);
-        }
+router.get('/researchResultes', (req, res) => {
+    var queryData = `SELECT frid,research_content_ko,research_name_ko FROM Research_Fields LIMIT 1 ORDER BY date desc`;
+    db.query(queryData, function (err, fields) {
+        var queryData1 = `SELECT img_src FROM Research_Fields_Photo WHERE qid=${fields.rfid}`;
+        db.query(queryData1, function (err, fields1) {
+            if (err) {
+                throw err;
+            }
+            else {
+                console.log(fields)
+                console.log(fields1)
+                res.json(fields, fields1);
+            }
+        })
     })
 })
 module.exports = router;
