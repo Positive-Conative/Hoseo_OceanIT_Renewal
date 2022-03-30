0dd4e5f7e48cd767c76dde8fa3fdc990cf8268f0
우성 홍 <20171302@vision.hoseo.edu>
2022-03-30 10:59:42 +0900

    Test

--- routes/research.js
+++ routes/research.js
@@ -1,67 +1,75 @@
 'use strict';
 
-var express = require('express');
-var router = express.Router();
-var androidController = require('../controller/androidController')
+var express             = require('express');
+var router              = express.Router();
+var AppCtrl             = require('../controller/androidController')
+var ResultCtrl          = require('../controller/researchResultsController');
+var FieldCtrl           = require('../controller/researchFieldsController');
+var PatentCtrl          = require('../controller/research/resultsPatentController')
+var TreatiseCtrl        = require('../controller/research/resultsTreatiseController ')
+var AnnouncementCtrl    = require('../controller/research/resultsAnnouncementController')
 
+//WEB
 //research fields
-var researchFieldsController = require('../controller/researchFieldsController');
-router.get('/fields', researchFieldsController.researchFields);
-router.get('/fields/detail', researchFieldsController.researchFieldsDetail);
-//android
-router.get('/android',androidController.Android);
-router.get('/android/fields', researchFieldsController.androidResearchFieldsAll);
-router.get('/fields/write', researchFieldsController.researchFieldhWrite);
+//----------------------------------------------------------------------------------//
+
+router.get('/fields', FieldCtrl.researchFields);
+router.get('/fields/detail', FieldCtrl.researchFieldsDetail);
+
+//App
+//research fields
+//----------------------------------------------------------------------------------//
+
+router.get('/android',AppCtrl.Android);
+router.get('/android/fields', FieldCtrl.androidResearchFieldsAll);
+router.get('/fields/write', FieldCtrl.researchFieldhWrite);
 
 //----------------------------------------------------------------------------------//
+
 //research results Patent
-var researchResultsController = require('../controller/researchResultsController');
-var resultsPatentController = require('../controller/results/resultsPatentController')
-
-router.get('/results/patent', resultsPatentController.researchResultsPatent);
-router.get('/results/patent/write', resultsPatentController.researchResultsPatentWrite)
-router.get('/results/patent/detail', resultsPatentController.researchResultsPatentDetail);
-router.get('/results/patent/update', resultsPatentController.researchResultsPatentUpdate);
-router.get('/results/patent/delete', resultsPatentController.researchResultsPatentDelete);
-// router.post('/results/patent/write', resultsPatentController.researchResultsPatentWriteP)
+router.get('/results/patent', PatentCtrl.researchResultsPatent);
+router.get('/results/patent/write', PatentCtrl.researchResultsPatentWrite)
+router.get('/results/patent/detail', PatentCtrl.researchResultsPatentDetail);
+router.get('/results/patent/update', PatentCtrl.researchResultsPatentUpdate);
+router.get('/results/patent/delete', PatentCtrl.researchResultsPatentDelete);
+// router.post('/results/patent/write', PatentCtrl.researchResultsPatentWriteP)
 
 //----------------------------------------------------------------------------------//
+
 //research results Treatise
-var resultsTreatiseController = require('../controller/results/resultsTreatiseController ')
-
-router.get('/results/treatise', resultsTreatiseController.researchResultsTreatise);
-router.get('/results/treatise/write', resultsTreatiseController.researchResultsTreatiseWrite);
-router.get('/results/treatise/detail', resultsTreatiseController.researchResultsTreatiseDetail);
-router.get('/results/treatise/update', resultsTreatiseController.researchResultsTreatiseUpdate)
-router.get('/results/treatise/delete', resultsTreatiseController.researchResultsTreatiseDelete)
-// router.post('/results/treatise/write', resultsTreatiseController.researchResultsTreatiseWriteP);
+router.get('/results/treatise', TreatiseCtrl.researchResultsTreatise);
+router.get('/results/treatise/write', TreatiseCtrl.researchResultsTreatiseWrite);
+router.get('/results/treatise/detail', TreatiseCtrl.researchResultsTreatiseDetail);
+router.get('/results/treatise/update', TreatiseCtrl.researchResultsTreatiseUpdate)
+router.get('/results/treatise/delete', TreatiseCtrl.researchResultsTreatiseDelete)
+// router.post('/results/treatise/write', TreatiseCtrl.researchResultsTreatiseWriteP);
 
 //----------------------------------------------------------------------------------//
+
 //research results Treatise
-var resultsAnnouncementController = require('../controller/results/resultsAnnouncementController')
-
-router.get('/results/announcement', resultsAnnouncementController.researchResultsAnnouncement);
-router.get('/results/announcement/write', resultsAnnouncementController.researchResultsAnnouncementWrite)
-router.get('/results/announcement/detail', resultsAnnouncementController.researchResultsAnnouncementDetail);
-router.get('/results/announcement/update', resultsAnnouncementController.researchResultsAnnouncementUpdate)
-router.get('/results/announcement/delete', resultsAnnouncementController.researchResultsAnnouncementDelete)
-// router.post('/results/announcement/write', resultsAnnouncementController.researchResultsAnnouncementWriteP)
+router.get('/results/announcement', AnnouncementCtrl.researchResultsAnnouncement);
+router.get('/results/announcement/write', AnnouncementCtrl.researchResultsAnnouncementWrite)
+router.get('/results/announcement/detail', AnnouncementCtrl.researchResultsAnnouncementDetail);
+router.get('/results/announcement/update', AnnouncementCtrl.researchResultsAnnouncementUpdate)
+router.get('/results/announcement/delete', AnnouncementCtrl.researchResultsAnnouncementDelete)
+// router.post('/results/announcement/write', AnnouncementCtrl.researchResultsAnnouncementWriteP)
 
 //----------------------------------------------------------------------------------//
-// router.get('/results/detail', researchResultsController.researchResultsDetail);
-router.get('/android/results', researchResultsController.androidResearchResultsAll);
 
-router.get('/results/write', researchResultsController.researcResultWrite);
+// router.get('/results/detail', ResultCtrl.researchResultsDetail);
+router.get('/android/results', ResultCtrl.androidResearchResultsAll);
+
+router.get('/results/write', ResultCtrl.researcResultWrite);
 
 //----------------------------------------------------------------------------------//
 //research POST
-router.post('/fields/write', researchFieldsController.researchFieldhWriteP);
-router.post('/results/write', researchResultsController.researcResultWriteP);
+router.post('/fields/write', FieldCtrl.researchFieldhWriteP);
+router.post('/results/write', ResultCtrl.researcResultWriteP);
 
 //----------------------------------------------------------------------------------//
 //research DELETE
 
-router.post('/fields/delete', researchFieldsController.researchFieldhDelete)
-// router.post('/results/delete',researchResultsController.researchResultDelete)
+router.post('/fields/delete', FieldCtrl.researchFieldhDelete)
+// router.post('/results/delete',ResultCtrl.researchResultDelete)
 
 module.exports = router;
